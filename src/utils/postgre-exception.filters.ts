07622369a7772617b3ba89305407ddfe3102aeae
Halil Sarıdede <54331060+hllibrhm01@users.noreply.github.com>
2024-03-06 00:38:17 +0300

    Update postgre-exception.filters.ts

--- src/utils/postgre-exception.filters.ts
+++ src/utils/postgre-exception.filters.ts
@@ -5,3 +5,20 @@ import {
   ArgumentsHost,
   HttpStatus
 } from "@nestjs/common";
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception instanceof Error ? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.BAD_REQUEST;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception instanceof Error ? exception.message : exception
+    });
+  }
+}
